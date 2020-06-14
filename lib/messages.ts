52a46067bfd4dd686126ecd5a9e208fb90b9a12b
Michael Ford <mtford@gmail.com>
2020-06-14 12:32:45 +0100

    Switch up the message payload types for ease of use

--- lib/messages.ts
+++ lib/messages.ts
@@ -1,15 +1,15 @@
 import { NativeModule, WatchPayload } from './native-module';
 import { _subscribeToNativeWatchEvent, NativeWatchEvent } from './events';
 
-export function sendMessage<MessageToWatch extends WatchPayload = WatchPayload,
-  MessageFromWatch extends WatchPayload = WatchPayload>(
+export function sendMessage<
+  MessageFromWatch extends WatchPayload = WatchPayload,
+  MessageToWatch extends WatchPayload = WatchPayload
+>(
   message: MessageToWatch,
   replyCb: (reply: MessageFromWatch) => void = (reply) => {
     console.warn(`Unhandled watch reply`, reply);
   },
-  errCb: (err: Error & {code?: string, domain?: string}) => void = (err) => {
-    console.error('Unhandled sendMessage error', err);
-  },
+  errCb: () => {},
 ) {
   NativeModule.sendMessage<MessageToWatch, MessageFromWatch>(
     message,
@@ -18,15 +18,18 @@ export function sendMessage<MessageToWatch extends WatchPayload = WatchPayload,
   );
 }
 
-export type WatchMessageListener<Payload = WatchPayload,
-  ResponsePayload = WatchPayload> = (
+export type WatchMessageListener<
+  ResponsePayload = WatchPayload,
+  Payload = WatchPayload> = (
   payload: Payload & { id?: string },
   // if the watch sends a message without a messageId, we have no way to respond
   replyHandler: ((resp: ResponsePayload) => void) | null,
 ) => void;
 
-export function subscribeToMessages<MessageFromWatch extends WatchPayload = WatchPayload,
-  MessageToWatch extends WatchPayload = WatchPayload>(cb: WatchMessageListener<MessageFromWatch, MessageToWatch>) {
+export function subscribeToMessages<
+  MessageToWatch extends WatchPayload = WatchPayload,
+  MessageFromWatch extends WatchPayload = WatchPayload,
+  >(cb: WatchMessageListener<MessageToWatch, MessageFromWatch>) {
   return _subscribeToNativeWatchEvent<NativeWatchEvent.EVENT_RECEIVE_MESSAGE,
     MessageFromWatch & { id?: string }>(NativeWatchEvent.EVENT_RECEIVE_MESSAGE, (payload) => {
     const messageId = payload.id;
