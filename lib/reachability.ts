89f276f0555b8a970c2df101fae966f6575a9943
Gabriel Pérez <gabriel@garox.org>
2020-07-22 11:49:40 -0400

    add subscribeToReachability back

--- lib/reachability.ts
+++ lib/reachability.ts
@@ -1,5 +1,13 @@
 import {NativeModule} from './native-module';
+import watchEvents, { UnsubscribeFn } from './events';
+
 
 export function getReachability(): Promise<boolean> {
   return NativeModule.getReachability();
+}
+
+
+export function subscribeToReachability(callback: (reachable: boolean) => void): UnsubscribeFn {
+  NativeModule.getReachability().then(callback)
+  return watchEvents.addListener('reachability', callback)
 }
