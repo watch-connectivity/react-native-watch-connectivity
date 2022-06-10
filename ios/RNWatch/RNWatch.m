1af4dae0115a0d488f4c729dd8ef941c3cd58f09
Jonas Sourlier <jonas.sourlier@yolp.ch>
2022-06-10 11:12:47 +0200

    propagating [userInfoTransfer userInfo] instead of whole userInfoTransfer

--- ios/RNWatch/RNWatch.m
+++ ios/RNWatch/RNWatch.m
@@ -502,7 +502,7 @@ RCT_EXPORT_METHOD(dequeueUserInfo:
 - (void)session:(WCSession *)session didFinishUserInfoTransfer:(WCSessionUserInfoTransfer *)userInfoTransfer error:(NSError *)error {
     if (error) {
         NSLog(@"Error: %@ %@", error, [error userInfo]);
-        [self dispatchEventWithName:EVENT_WATCH_USER_INFO_ERROR body:@{@"userInfoTransfer": userInfoTransfer, @"error": [error userInfo]}];
+        [self dispatchEventWithName:EVENT_WATCH_USER_INFO_ERROR body:@{@"userInfo": [userInfoTransfer userInfo], @"error": [error userInfo]}];
     }
 }
 
