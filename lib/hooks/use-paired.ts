e5743bd61da982a5495918f653009a09a2a6a047
Mark Descalzo <mdescalzo@mac.com>
2022-06-16 14:46:19 -0600

    Update use-paired.ts
    Added line to set initial value based on current pair state

--- lib/hooks/use-paired.ts
+++ lib/hooks/use-paired.ts
@@ -1,10 +1,12 @@
 import {useEffect, useState} from 'react';
 import watchEvents from '../events';
+import {getIsPaired} from '../paired';
 
 export function usePaired() {
   const [paired, setPaired] = useState(false);
 
   useEffect(() => {
+    getIsPaired().then(setPaired);
     return watchEvents.addListener('paired', setPaired);
   }, []);
 
