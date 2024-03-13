9de34e46c8df253e0987a09c694888bc5551cfef
cristianceban <cebancristi4444@gmail.com>
2024-03-13 13:58:15 +0200

    feat(expo-plugin): add docs

--- plugin/src/ios/getFiles.ts
+++ plugin/src/ios/getFiles.ts
@@ -34,5 +34,19 @@ export function getFilesForXcode(filesPath: string): WidgetFiles {
     });
   }
 
+  const allFiles = [...widgetFiles.resourcesFiles, ...widgetFiles.sourceFiles];
+
+  if (!allFiles.find((file) => file.includes('Info.plist'))) {
+    console.warn(
+      'Info.plist file is required for the watch, please add it to the folder declared as `sourceDir` in `app.json`',
+    );
+  }
+
+  if (!allFiles.find((file) => file.includes('Assets.xcassets'))) {
+    console.warn(
+      'Assets.xcassets file is required for the watch, please add it to the folder declared as `sourceDir` in `app.json`',
+    );
+  }
+
   return widgetFiles;
 }
